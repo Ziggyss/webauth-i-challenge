b40b54c2e2d9ed5473c5a80d6ca8ac99b81b252f
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-12 17:01:37 +0000

    short note

--- routers/restrictedRouter.js
+++ routers/restrictedRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/*", restricted, (req, res) => {
+router.get("/*", restricted, (req, res) => {  //I got quite excited as I found two ways to make the path work - :pathname or * both seem to work fine...
   res.send("Restricted path working");
 });
 
