f7f24e49915b5bbcc113621dae298d58f6d9f7c2
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-12 13:39:36 +0000

    logout added

--- server.js
+++ server.js
@@ -86,6 +86,20 @@ server.post("/api/login", (req, res) => {
     });
 });
 
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.json('Nope, you cannot log out...')
+      } else {
+        res.json('Goodbye, see you next time!')
+      }
+    })
+  } else {
+    res.end();
+  }
+})
+
 function restricted(req, res, next) {
   if (req.session && req.session.user) {
     next();
