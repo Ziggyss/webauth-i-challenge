af35b4e885b1988219bad4feed0956e0593b206f
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-12 13:20:58 +0000

    set up express-session and refactor restricted middlware

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
 const server = express();
 const Users = require("./users/user-model");
 
@@ -59,19 +64,11 @@ server.post("/api/login", (req, res) => {
 });
 
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: "You shall not pass!" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: err.message });
-    });
-}
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(400).json({ message: 'You shall not pass!' });
+  }
+};
 
 module.exports = server;
